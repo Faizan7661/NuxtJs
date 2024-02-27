31801c79a686a325ef550ff05632cb07ca1a8ade
Mohammed Faizan <mohammedfaizan7661@gmail.com>
2024-02-27 13:09:54 +0530

    NuxtJs Fundamentals

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, reiciendis totam! Fugit beatae delectus ab. Modi quaerat eum laboriosam ipsam?</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum cum atque laboriosam dignissimos quos beatae ullam ipsum rem? Dolorem, ea.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
